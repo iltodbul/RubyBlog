68ff3923ccfc9a78f86abd4950c2e14e4b34101f
Iliya Todorov <53480117+iltodbul@users.noreply.github.com>
2023-02-25 20:52:51 +0200

    Add Validations and Displaying Error Messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,29 @@
 class ArticlesController < ApplicationController
   def index
-    # debugger
     @articles = Article.all
   end
 
   def show
     @article = Article.find(params[:id])
   end
-end
 
-def new
-  @article = Article.new
-end
+  def new
+    @article = Article.new
+  end
 
-def create
-  @article = Article.new(title: "...", body: "...")
+  def create
+    @article = Article.new(article_params)
 
-  if @article.save
-    redirect_to @article
-  else
-    render :new, status: :unprocessable_entity
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :body)
   end
 end
